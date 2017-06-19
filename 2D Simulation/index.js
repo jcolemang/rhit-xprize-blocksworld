c00d255de6b303ebc3badcf1c213f54cdc386ab6
YukariDyy <dingy2@rose-hulman.edu>
2017-06-19 13:24:46 -0400

    Live audio (#23)
    * Live Audio file, save audio on the server
    
    * add live audio file
    
    * Make connection between the main page and the live audio
    
    * Delete unused files
    
    * Update introductions

--- 2D Simulation/index.js
+++ 2D Simulation/index.js
@@ -13,7 +13,8 @@ var letters = ["A", "B", "C", "D", "E", "F", "G"];
 var color = ['red', 'blue','green','orange','yellow'];
 var taskID = 0;
 var task = [];
-var introductions = [];
+var introductions1 = [];
+var introductions2 = [];
 var br; var bm; var pn; var pp; var p; var te; var ie;
 var startTime, endTime;
 var startPosMap = {};
@@ -41,28 +42,15 @@ function initInstructions() {
     task.push("Matching");
     task.push("Searching");
     task.push("Rainbow");
-    introductions.push("Sorting:<br><br>User1 Instructions:<br>&emsp;You have to give the other user an instruction to sort the blocks in a particular way." +
-        "<br>&emsp;When you speak, the converted text will show up in the text box.<br>Once the task is complete, click the end button.<br><br>User2 " +
-        "Instructions:<br>&emsp;The other user will give you instructions to complete the task.<br>&emsp;Try to complete it in the minimum number of block moves possible.<br>");
 
-    introductions.push("Matching:<br><br>User1 Instructions:<br>&emsp;You have to give the other user an instruction to try to match the blocks according to some criterion" +
-        ".<br>&emsp;When you speak, the converted text will show up in the text box.<br>&emsp;After giving the instruction, you and the other user will simultaneously flip " +
-        "two separate blocks. If the blocks match, you should drag both the blocks to the left. If they don’t, the other user will drag both the blocks to the right" +
-        ".<br>&emsp;Once the all the blocks have been dragged to either side, click the end button.<br>&emsp;If one block is left at the end, drag it to your side and " +
-        "click the end button.<br><br>User2 Instructions:<br>&emsp;The other user will give you instructions to complete the task.<br>&emsp;Try to complete it in the " +
-        "minimum number of block moves possible.");
-
-    introductions.push("Searching:<br><br>User1 Instructions:<br>&emsp;Select two related words.<br>&emsp;Tell the other user which one of those words you are going to " +
-        "search and which ones they are supposed to search.<br>&emsp;Type in the word in double quotes into the give textbox named “Words”.<br>&emsp;When you speak, " +
-        "the converted text will show up in the text box.<br>&emsp;Try to complete the task in the minimum number of block moves possible.<br>&emsp;Once the task is " +
-        "complete, click the end button.<br>&emsp; Try to complete the task in the minimum number of block moves possible.<br><br>User2 Instructions:<br>&emsp;The other " +
-        "user will give you instructions to complete the task.<br>&emsp;Try to complete it in the minimum number of block moves possible.<br>&emsp;");
-
-    introductions.push("Rainbow:<br><br>User1 Instructions:<br>&emsp;You will make a pattern in the separate window labelled “Draw”.<br>&emsp;Then instruct the other user " +
-        "to assist you in replicating the same pattern in the other window.<br>&emsp;Once you have replicated the pattern, click the end button.<br>&emsp;Try to complete " +
-        "the task in the minimum number of block moves possible.<br><br>User2 Instructions:<br>&emsp;<br>&emsp;The other user will give you instructions to complete the " +
-
-        "task.<br>&emsp;Try to complete it in the minimum number of block moves possible.<br>&emsp;");
+    introductions1.push("Sorting:<br>&emsp;Your task is to sort the blocks in a particular way.<br>User1 Instructions:<br>&emsp;You will decide how you want to sort the block and instruct your partner to get help finishing the task, by speaking into your device’s microphone, or by typing in the text box. You can only speak/type after pressing the start button. Your partner will be able to hear what you say and see what you type. You can also use gestures to point to the intended card/position to communicate effectively. You can use these gestures while speaking or at any time while playing the game.<br>&emsp;The partner will assist you in the sorting task according to your instructions but only you can speak to him. The partner can’t speak back. Both of you will be able to see anything either of you do on the table.");
+    introductions2.push("Sorting:<br>&emsp;Your task is to sort the blocks in a particular way.<br>User2 Instructions:<br>&emsp;Your partner will give you instructions to complete the task. Your partner might also point while speaking or at any time while playing the game so you must pay attention to them and try to cooperate.You have to follow your partner’shis instructions and try to assist your partnerhim in the best possible way. Both of you will will be able to see anything either of you do on the table.<br>");
+    introductions1.push("Matching:<br>Your task is to match the blocks in a particular way. Once the game starts, both of you and your partner will simultaneously flip two separate blocks. If the blocks match, user1 should drag both the blocks to the left. If they don’t, the user2 will drag both the blocks to the right.Your goal is to flip all the blocks. If one block is left at the end, user1 will drag it to the left and click the end button.<br>User1 Instructions:<br>&emsp;You will decide by what criteria you want to match the cards and instruct your partner in order to get help finishing the task, by speaking into your device’s microphone, or by typing in the text box. You can only speak/type after pressing the start button. Your partner will be able to hear what you say and see what you type. You can also use gestures to point to the intended card/position to communicate effectively. You can use these gestures while speaking or at any time while playing the game.<br>&emsp;Your partner will be able to hear what you say and see what you type but they will not be able to talk or type to you. Both of you will see the outcomes of any changes either of you make to the table.<br>");
+    introductions2.push("Matching:<br>Your task is to match the blocks in a particular way. Once the game starts, both of you and your partner will simultaneously flip two separate blocks. If the blocks match, user1 should drag both the blocks to the left. If they don’t, the user2 will drag both the blocks to the right.Your goal is to flip all the blocks. If one block is left at the end, user1 will drag it to the left and click the end button.<br>User2 Instructions:<br>&emsp;Your partner will give you instructions to complete the task. Your partner might also use gestures while speaking or at any time while playing the game so you must pay attention to them, try to understand what those gestures mean and do work accordingly. You have to follow his instructions and try to assist him in the best possible way. Both of you will see the outcomes of any changes either of you make to the table.<br>");
+    introductions1.push("Searching:<br>Your task is to search for a word. You can use whatever way you want to represent that you have made your word. It doesn’t matter if you and your partner use different ways to represent this. It should be clear to anyone without talking to you that you have spelled out a word. Click the start button to start game when you are ready to do the task. Further instruction are here below:<br>User1 Instructions:<br>&emsp;Select two related words after looking at all the cards. Type in the word into the textboxes, “word for user 1” (your word) and “word for user 2”(partner’s word).<br>&emsp;Tell your partner which one of those words you are going to search and which ones they are supposed to search. You can only speak/type after pressing the start button. Your partner will be able to hear what you say and see what you type.<br>&emsp;You can also use gestures to point to the intended card/position to communicate effectively. You can use these gestures while speaking or at any time while playing the game. Both of you will see the outcomes of any changes either of you make to the table.<br>");
+    introductions2.push("Searching:<br>Your task is to search for a word. You can use whatever way you want to represent that you have made your word. It doesn’t matter if you and your partner use different ways to represent this. It should be clear to anyone without talking to you that you have spelled out a word. Click the start button to start game when you are ready to do the task. Further instruction are here below:<br>User2 Instructions:<br>&emsp;Your partner will give you instructions to complete the task. Your partner might also use gestures while speaking or at any time while playing the game so you must pay attention to them, try to understand what those gestures mean and do work accordingly. You have to follow his instructions and try to assist him in the best possible way. Both of you will see the outcomes of any changes either of you make to the table.<br>");
+    introductions1.push("Rainbow:<br>You need to draw a specific pattern (like a triangle) using the cards, along with your partner.<br>User1 Instructions:<br>&emsp;You will decide what pattern to make and instruct your partner to get help finishing the task, by speaking into your device’s microphone, or by typing in the text box. You can only speak/type after pressing the start button. Your partner will be able to hear what you say and see what you type. You can also use gestures to point to the intended card/position to communicate effectively. You can use these gestures while speaking or at any time while playing the game.<br>&emsp;The partner will assist you in the task according to your instructions but only you can speak to him. He can’t speak back. Both of you will see the outcomes of any changes either of you make to the table.<br>");
+    introductions2.push("Rainbow:<br>You need to draw a specific pattern (like a triangle) using the cards, along with your partner.<br>User2 Instructions:<br>&emsp;Your partner will give you instructions to complete the task. Your partner might also use gestures while speaking or at any time while playing the game so you must pay attention to them, try to understand what those gestures mean and do work accordingly. You have to follow his instructions and try to assist him in the best possible way. Both of you will see the outcomes of any changes either of you make to the table.<br>");
 }
 
 
@@ -102,6 +90,7 @@ function incrementGesture(e) {
     gestureCount++;
     property.innerText = gestureCount;
     var gestureElement = document.getElementById("gestureToggle");
+
     gestureElement.style.left = e.clientX+'px';
     gestureElement.style.top = e.clientY+'px';
     gestureElement.style.visibility = "visible";
@@ -214,6 +203,7 @@ function StartGame() {
         showChosenStuff();
     }
     startTime = new Date().getTime();
+    startPeer(1);
 }
 
 function endGame() {
