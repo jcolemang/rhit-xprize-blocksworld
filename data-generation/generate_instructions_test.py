18e7de6e03aa6f088e89ac2e28a593650ad41f6f
Lewis-Kelley <kelleyld@rose-hulman.edu>
2017-12-17 20:40:38 -0500

    Added tests for Configuration::is_complete

--- data-generation/generate_instructions_test.py
+++ data-generation/generate_instructions_test.py
@@ -1,5 +1,9 @@
 import generate_instructions as gi
 
+"""
+Block tests
+"""
+
 def test_block_inequality_with_generated_ids():
     assert gi.Block('A', 'BLUE', 'B', 'GREEN', (0, 0)) \
         != gi.Block('A', 'BLUE', 'B', 'GREEN', (0, 0))
@@ -51,3 +55,17 @@ def test_block_flip_swaps_visuals():
     assert base_block.side1_color == flipped_block.side2_color
     assert base_block.side2_letter == flipped_block.side1_letter
     assert base_block.side2_color == flipped_block.side1_color
+
+
+"""
+Configuration
+"""
+
+def test_is_complete_empty_current_empty_final():
+    assert gi.Configuration([], []).is_complete()
+
+def test_is_complete_nonempty_current_empty_final():
+    assert not gi.Configuration([1], []).is_complete()
+
+def test_is_complete_nonempty_current_nonempty_final():
+    assert not gi.Configuration([1], [2]).is_complete()
