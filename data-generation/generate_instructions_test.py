a9e4a8359aeabf49fdd6ce894dbbd3b721aa4026
Lewis-Kelley <kelleyld@rose-hulman.edu>
2017-12-17 20:04:26 -0500

    Added tests for Block::flip

--- data-generation/generate_instructions_test.py
+++ data-generation/generate_instructions_test.py
@@ -29,3 +29,25 @@ def test_block_shift_to_updates_pos():
     shifted_block = base_block.shift_to(new_pos)
 
     assert shifted_block.position == new_pos
+
+def test_block_flip_keeps_block_id():
+    base_block = gi.Block('A', 'BLUE', 'B', 'GREEN', (0, 0))
+    flipped_block = base_block.flip()
+
+    assert base_block.block_id == flipped_block.block_id
+
+
+def test_block_flip_keeps_pos():
+    base_block = gi.Block('A', 'BLUE', 'B', 'GREEN', (0, 0))
+    flipped_block = base_block.flip()
+
+    assert base_block.position == flipped_block.position
+
+def test_block_flip_swaps_visuals():
+    base_block = gi.Block('A', 'BLUE', 'B', 'GREEN', (0, 0))
+    flipped_block = base_block.flip()
+
+    assert base_block.side1_letter == flipped_block.side2_letter
+    assert base_block.side1_color == flipped_block.side2_color
+    assert base_block.side2_letter == flipped_block.side1_letter
+    assert base_block.side2_color == flipped_block.side1_color
