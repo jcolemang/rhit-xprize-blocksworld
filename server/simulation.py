d30ac7e917b15a1e6535355fcebb384f892309ed
Lewis-Kelley <kelleyld@rose-hulman.edu>
2018-01-16 22:57:59 -0500

    Server now able to open a websocket with the client over socket.io

--- server/simulation.py
+++ server/simulation.py
@@ -1,9 +1,16 @@
 import sys
+import socketio
+import tornado.web
+import eventlet
 
 import config as cfg
 
 def main():
     config = cfg.generate_config(get_is_local())
+    sio = socketio.Server()
+    app = tornado.web.Application()
+    socketApp = socketio.Middleware(sio, app)
+    eventlet.wsgi.server(eventlet.listen(('', 8080)), socketApp)
 
 def get_is_local():
     args = sys.argv[1:]
