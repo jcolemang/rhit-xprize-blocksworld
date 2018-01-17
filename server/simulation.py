05c298f892d8fe38986127935983aefdddf34ac7
Lewis-Kelley <kelleyld@rose-hulman.edu>
2018-01-17 08:32:09 -0500

    Added proper socket response upon connecting.

--- server/simulation.py
+++ server/simulation.py
@@ -5,12 +5,19 @@ import eventlet
 
 import config as cfg
 
+sio = socketio.Server()
+
 def main():
     config = cfg.generate_config(get_is_local())
-    sio = socketio.Server()
     app = tornado.web.Application()
     socketApp = socketio.Middleware(sio, app)
-    eventlet.wsgi.server(eventlet.listen(('', 8080)), socketApp)
+    eventlet.wsgi.server(eventlet.listen(('', config['serverPort'])), socketApp)
+
+@sio.on('connect')
+def connection_handler(sid, msg):
+    print("Connected to client")
+    sio.enter_room(sid, 'Room0')
+    sio.emit('freeze_start')
 
 def get_is_local():
     args = sys.argv[1:]
