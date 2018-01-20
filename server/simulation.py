dbf0c4a344eeddf9ec2967bfb674ec5bdf598a10
Lewis-Kelley <kelleyld@rose-hulman.edu>
2018-01-19 21:38:45 -0500

    Refactored out simple echo emits into a separate file.

--- server/simulation.py
+++ server/simulation.py
@@ -5,31 +5,24 @@ import eventlet
 
 import config as cfg
 from rooms import RoomsTracker
+from echo_emitter import EchoEmitter
 
 sio = socketio.Server()
 rooms_tracker = RoomsTracker(sio)
 
 def main():
     config = cfg.generate_config(get_is_local())
-    setup_echos()
-    setup_updates()
+    echo_emitter = EchoEmitter(sio, rooms_tracker)
+    setup_emits(echo_emitter)
     start_app(config)
 
 def get_is_local():
     args = sys.argv[1:]
     return len(args) == 0 or args[0] == 'local'
 
-def setup_echos():
-    echo_event('enable_blocks_for_player_2')
-    echo_event('disable_blocks_for_player_2', True)
-    echo_event('Update_score')
-
-def setup_updates():
-    update_on_receive('position')
-    update_on_receive('flip_block')
-    update_on_receive('movement_data')
-    update_on_receive('gesture_data')
-    update_on_receive('user_message')
+def setup_emits(echo_emitter):
+    echo_emitter.setup_echos()
+    echo_emitter.setup_updates()
 
 def start_app(config):
     app = tornado.web.Application()
@@ -41,21 +34,6 @@ def connection_handler(sid, _):
     print("Connected to client")
     rooms_tracker.add_to_room(sid)
     sio.emit('freeze_start')
-
-def echo_event(event, ignore_data=False):
-    def echo_handler(sid, data):
-        if ignore_data:
-            data = None
-
-        sio.emit(event, data, rooms_tracker.get_room(sid))
-
-    sio.on(event, echo_handler)
-
-def update_on_receive(event):
-    def update_handler(sid, data):
-        sio.emit("update_" + event, data, rooms_tracker.get_room(sid))
-
-    sio.on("receive_" + event, update_handler)
 
 if __name__ == '__main__':
     main()
