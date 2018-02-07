d62e174da3726ca657cf8fa1c7c314bccf7cdc7e
Coleman Gibson <gibsonjc@rose-hulman.edu>
2018-02-06 19:08:16 -0500

    Added start of model interface

--- server/neural_network_interface.py
+++ server/neural_network_interface.py
@@ -1,18 +1,34 @@
+from keras.models import load_model
+
 _movement_count_dict = {}
 
-def generate_move(sid, gesture_data, message_data):
-    """Returns the (position data, movement count) that will be sent to the
-    client.
+class BlocksworldModel:
+    def __init__(self):
+        pass
 
-    """
-    movement_ct = _movement_count_dict.setdefault(sid, 0) + 1
-    _movement_count_dict[sid] = movement_ct
+    def generate_move(sid, gesture_data, message_data):
+        """Returns the (position data, movement count) that will be sent to the
+        client.
 
-    if gesture_data is None:
-        gesture_data = {'top': 0,
-                        'left': 0}
+        """
+        raise NotImplementedError('Subclasses must implement generate_move')
 
-    return ({'top': gesture_data['top'],
-             'left': gesture_data['left'],
-             'block_id': 'block3'},
-            movement_ct)
+
+class DumbBlocksworldModel(BlocksworldModel):
+    def generate_move(sid, gesture_data, message_data):
+        movement_ct = _movement_count_dict.setdefault(sid, 0) + 1
+        _movement_count_dict[sid] = movement_ct
+
+        if gesture_data is None:
+            gesture_data = {'top': 0,
+                            'left': 0}
+
+        return ({'top': gesture_data['top'],
+                 'left': gesture_data['left'],
+                 'block_id': 'block3'},
+                movement_ct)
+
+class NeuralNetworkBlocksworldModel(BlocksworldModel):
+    def __init__(self, h5_path):
+        self.model = load_model(h5_path)
+        
