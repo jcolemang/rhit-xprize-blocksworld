6c51b110f557ecf564d95d9fd949e72910490902
Lewis Kelley <kelleyld@rose-hulman.edu>
2018-03-27 10:22:29 -0400

    Ai database storage (#46)
    Close #40 
    
    * Small renaming refactors. Removed extra z variable for css z-axis.
    
    * Refactors around update_position.
    
    * Removed global variable block_actions to add singleton movesTracker.
    
    * Moved global variable type into movesTracker.
    
    * Added start and end into movesTracker.
    
    * Removed move recording from the tutorial.
    
    * Moved interval into movesTracker.
    
    * Moved movement_startpos and movement_endpos into movesTracker.
    
    * Moved instructions into movesTracker.
    
    * Moved gesture tracking into movesTracker.
    
    * Isolated addition of movesTracker fields into movesTracker itself.
    
    * Moved players into movesTracker.
    
    * Moved players tracking into movesTracker
    
    * Made add_block_action private in movesTracker.
    
    * Moved the exporting of actions into movesTracker.
    
    * Fixed block flips.
    
    * Client now transmits data to the server for storage in database.
    
    The moves are not being properly recorded, however.
    
    * Now stores movement actions.
    
    * Removed now unnecessary init function.
    
    * Removed debugging print statement.

--- 2D Simulation/initBlocks.js
+++ 2D Simulation/initBlocks.js
@@ -75,18 +75,13 @@ function initBlocks(config) {
         $("#block" + i).bind("contextmenu", function(e) {
             if (!((taskID == 3 || taskID == 0) && am_i_player1)) {
                 var event = e || window.event;
-                flipBlock('block' + $(this).data("id"), event, bLetters, bColors);
-                send_flip_to_server('block' + $(this).data("id"));
-                if (am_i_player1) {
-                    players.push("Human");
-                } else {
-                    players.push("Robot");
-                }
-
                 var flipped_block_color = document.getElementById("block" + $(this).data("id")).style.backgroundColor;
                 var flipped_block_letter = blockLetters[$(this).data("id")];
 
-                block_actions.push("Block id: " + $(this).data("id") + " " + "Letter: " + flipped_block_letter + " " + "Color: " + flipped_block_color);
+                flipBlock('block' + $(this).data("id"),
+                          flipped_block_letter, flipped_block_color,
+                          event, bLetters, bColors);
+                send_flip_to_server('block' + $(this).data("id"));
             }
         });
     }
