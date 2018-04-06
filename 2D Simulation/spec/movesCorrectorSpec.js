28d367f9db816c643f711ac045e23bcb0b3e62c5
Lewis-Kelley <kelleyld@rose-hulman.edu>
2018-04-06 08:48:03 -0400

    Added tests for correct_move.

--- 2D Simulation/spec/movesCorrectorSpec.js
+++ 2D Simulation/spec/movesCorrectorSpec.js
@@ -1,7 +1,8 @@
 describe("movesCorrector", () => {
     let movesCorrector = require('../movesCorrector.js');
     correctionUI = {
-        display_flip_explanation: () => undefined
+        display_flip_explanation: () => undefined,
+        display_move_explanation: () => undefined
     };
 
     describe("when correcting a flip", () => {
@@ -18,6 +19,23 @@ describe("movesCorrector", () => {
 
         it("should display flip explanation", () => {
             expect(correctionUI.display_flip_explanation).toHaveBeenCalled();
+        });
+    });
+
+    describe("when correcting a move", () => {
+        beforeEach(() => {
+            spyOn(movesCorrector, "_start_correct_action");
+            spyOn(correctionUI, "display_move_explanation");
+
+            movesCorrector.correct_move();
+        });
+
+        it("should start a correction action", () => {
+            expect(movesCorrector._start_correct_action).toHaveBeenCalled();
+        });
+
+        it("should display move explanation", () => {
+            expect(correctionUI.display_move_explanation).toHaveBeenCalled();
         });
     });
 });
