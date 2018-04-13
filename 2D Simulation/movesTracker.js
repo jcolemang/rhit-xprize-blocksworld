f8c16fccc72137d4cf028a046d01d470f782453c
Lewis-Kelley <kelleyld@rose-hulman.edu>
2018-04-12 20:26:53 -0400

    Refactored to better use objects.

--- 2D Simulation/movesTracker.js
+++ 2D Simulation/movesTracker.js
@@ -1,99 +1,117 @@
-let movesTracker = new function () {
+function MovesTracker() {
     this.actions = [];
 
     this.add_gesture = function (left_pos_percent, top_pos_percent) {
-        this.actions.push({
-            end_date: getDateTime(),
-            left_pos: left_pos_percent,
-            top_pos: top_pos_percent,
-            export_func: export_gesture
-        });
-    }
-
-    function export_gesture(action) {
-        return "Gesture" + " "
-            + action.end_date + " "
-            + export_position(action.left_pos, action.top_pos);
+        this.actions.push(new _Gesture(left_pos_percent, top_pos_percent));
     }
 
     this.add_instruction = function (start_date, start_time, text) {
-        this.actions.push({
-            start_date: start_date,
-            end_date: getDateTime(),
-            interval: new Date().getTime() - start_time,
-            text: text,
-            export_func: export_instruction
-        });
-    }
-
-    function export_instruction(action) {
-        return "Instruction" + " "
-            + action.start_date + " "
-            + action.end_date + " "
-            + action.interval + " "
-            + action.text;
+        this.actions.push(new _Instruction(start_date, start_time, text));
     }
 
     this.add_flip = function (id, letter, color, x_pos, y_pos) {
-        this.actions.push({
-            id: id,
-            letter: letter,
-            color: color,
-            end_date: getDateTime(),
-            left_pos: x_pos,
-            top_pos: y_pos,
-            export_func: export_flip
-        });
-    }
-
-    function export_flip(action) {
-        return "Flip" + " "
-            + export_block_action(action) + " "
-            + action.end_date + " "
-            + export_position(action.left_pos, action.top_pos);
+        this.actions.push(new _Flip(id, letter, color, x_pos, y_pos));
     }
 
     this.add_move = function (id, letter, color,
                               orig_left, orig_top,
                               end_left, end_top) {
-        this.actions.push({
-            id: id,
-            letter: letter,
-            color: color,
-            end_date: getDateTime(),
-            left_pos: orig_left,
-            top_pos: orig_top,
-            new_left_pos: end_left,
-            new_top_pos: end_top,
-            export_func: export_move
-        });
-    }
-
-    function export_move(action) {
-        return "Movement" + " "
-            + export_block_action(action) + " "
-            + action.end_date + " "
-            + export_position(action.left_pos, action.top_pos) + " "
-            + export_position(action.new_left_pos, action.new_top_pos);
-    }
-
-    function export_position(left_pos, top_pos) {
-        return "(" + left_pos + "," + top_pos + ")";
-    }
-
-    function export_block_action(action) {
-        return "Block id: " + action.id + " "
-            + "Letter: " + action.letter + " "
-            + "Color: " + action.color;
+        this.actions.push(new _Move(id, letter, color,
+                                    orig_left, orig_top,
+                                    end_left, end_top));
     }
 
     this.export_actions = function () {
         let action_strs = [];
 
         for (let action of this.actions) {
-            action_strs.push(action.export_func(action));
+            action_strs.push(action.export_to_string());
         }
 
         return action_strs;
     }
 }
+
+function _Gesture(left_pos_percent, top_pos_percent) {
+    this.end_date = getDateTime();
+    this.left_pos = left_pos_percent;
+    this.top_pos = top_pos_percent;
+
+    this.export_to_string = function () {
+        return "Gesture" + " "
+            + this.end_date + " "
+            + _export_position(this.left_pos, this.top_pos);
+    };
+}
+
+function _Instruction(start_date, start_time, text) {
+    this.start_date = start_date;
+    this.end_date = getDateTime();
+    this.interval = new Date().getTime - start_time;
+    this.text = text;
+
+    this.export_to_string = function () {
+        return "Instruction" + " "
+            + this.start_date + " "
+            + this.end_date + " "
+            + this.interval + " "
+            + this.text;
+    };
+}
+
+function _Flip(id, letter, color, x_pos, y_pos) {
+    this.id = id;
+    this.letter = letter;
+    this.color = color;
+    this.end_date = getDateTime();
+    this.left_pos = x_pos;
+    this.top_pos = y_pos;
+
+    this.export_to_string = function () {
+        return "Flip" + " "
+            + _export_block_action(this) + " "
+            + this.end_date + " "
+            + _export_position(this.left_pos, this.top_pos);
+    }
+}
+
+function _Move(id, letter, color, orig_left, orig_top, end_left, end_top) {
+    this.id = id;
+    this.letter = letter;
+    this.color = color;
+    this.left_pos = orig_left;
+    this.top_pos = orig_top;
+    this.new_left_pos = end_left;
+    this.new_top_pos = end_top;
+
+    this.export_to_string = function () {
+        return "Movement" + " "
+            + _export_block_action(this) + " "
+            + this.end_date + " "
+            + _export_position(this.left_pos, this.top_pos) + " "
+            + _export_position(this.new_left_pos, this.new_top_pos);
+    };
+}
+
+function _export_block_action(action) {
+    return "Block id: " + action.id + " "
+        + "Letter: " + action.letter + " "
+        + "Color: " + action.color;
+}
+
+function _export_position(left_pos, top_pos) {
+    return "(" + left_pos + "," + top_pos + ")";
+}
+
+if (typeof module !== 'undefined'
+    && module.hasOwnProperty('exports')) {
+    module.exports = {
+        MovesTracker: MovesTracker,
+        _Gesture: _Gesture,
+        _Instruction: _Instruction,
+        _Flip: _Flip,
+        _Move: _Move
+    };
+}
+
+let movesTracker = new MovesTracker();
