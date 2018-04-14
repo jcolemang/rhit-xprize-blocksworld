a6f9d0f08b63f7dc757c615487fe8a223c64660b
Lewis-Kelley <kelleyld@rose-hulman.edu>
2018-04-14 17:20:18 -0400

    Removed unused text blobs.

--- 2D Simulation/index.js
+++ 2D Simulation/index.js
@@ -63,9 +63,6 @@ console.log('Block Colors: ', blockColors);
 
 var rainbow_select = 0;
 
-var generalintro = "General Instructions:<br>&emsp;In this game, you will see a table of two-sided blocks with different colors and letters on each side. You will be paired with a partner and given a task. Click the start button to start the game when you are ready to do the task. Once the task is complete, click the end button. Try to complete the task as efficiently as possible.<br>";
-var blockintro = "Block instructions:<br>&emsp;Mouse right click: flips block<br>&emsp;Mouse left double click: This acts like pointing to a position on the table.<br>&emsp;Whenever you do this, the gestures box count increases by 1 and a small black block appears at the position of the gesture.<br>&emsp;Mouse left click and drag block to another position: moves block to another position.<br>&emsp;Whenever you do this, the movement box count increases by 1.<br>";
-
 function getBlockStyle(blockColors) {
     let doc = '';
     for (var i = 0; i < NumBlocks ;i++) {
