c8251b081c33916220ca1b2ef8a9c6bd39ea8e18
Lewis-Kelley <kelleyld@rose-hulman.edu>
2018-04-17 10:28:55 -0400

    Trying Chrome instead of Firefox

--- 2D Simulation/conf.js
+++ 2D Simulation/conf.js
@@ -3,6 +3,6 @@ exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: ['e2eTesting.js'],
     capabilities: {
-        browserName: 'firefox'
+        browserName: 'chrome'
     }
 }
