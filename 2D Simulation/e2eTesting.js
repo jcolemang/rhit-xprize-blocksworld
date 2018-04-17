d8369aee609a0b52d48d5dbe5c51409c1ced76e7
Lewis-Kelley <kelleyld@rose-hulman.edu>
2018-04-17 18:44:10 -0400

    Added more simple E2E tests.

--- 2D Simulation/e2eTesting.js
+++ 2D Simulation/e2eTesting.js
@@ -3,20 +3,38 @@ describe("Blocksworld", () => {
     let long_timeout = 5000;
     let short_timeout = 1000;
 
+    let startButton;
+    let endButton;
+
     beforeEach(() => {
         browser.waitForAngularEnabled(false);
         browser.get("http://localhost:8000/game.html");
+
+        startButton = element(by.id('buttonStart'));
+        endButton = element(by.id('buttonEnd'));
     });
 
     afterEach(() => browser.refresh());
 
     it("should activate the start button", () => {
-        let startButton = element(by.id('buttonStart'));
-
         expect(startButton.isEnabled()).toEqual(true);
     });
 
     it("should have the correct window title", () => {
         expect(browser.getTitle()).toEqual('Blocks World');
+    });
+
+    describe("After starting the game", () => {
+        beforeEach((done) => {
+            startButton.click().then(done);
+        });
+
+        it("should deactivate the start button", () => {
+            expect(startButton.isEnabled()).toEqual(false);
+        });
+
+        it("should activate the end button", () => {
+            expect(endButton.isEnabled()).toEqual(true);
+        });
     });
 });
