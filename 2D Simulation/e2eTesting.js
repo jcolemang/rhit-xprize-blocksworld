ca50f1f21c3e30b4eec522980b5c8c96f8dc9315
Lewis-Kelley <kelleyld@rose-hulman.edu>
2018-04-17 19:28:34 -0400

    Made something resembling a fixed initial setup for the E2E tests.

--- 2D Simulation/e2eTesting.js
+++ 2D Simulation/e2eTesting.js
@@ -1,30 +1,37 @@
 describe("Blocksworld", () => {
     let EC = protractor.ExpectedConditions;
     let long_timeout = 5000;
-    let short_timeout = 1000;
+    let short_timeout = 500;
 
     let startButton;
     let endButton;
 
     beforeEach(() => {
         browser.waitForAngularEnabled(false);
-        browser.get("http://localhost:8000/game.html");
+        browser.get("http://localhost:8000/game.html?config=fixed");
 
         startButton = element(by.id('buttonStart'));
         endButton = element(by.id('buttonEnd'));
+        browser.sleep(short_timeout);
     });
 
-    afterEach(() => browser.refresh());
-
-    it("should activate the start button", () => {
-        expect(startButton.isEnabled()).toEqual(true);
+    afterEach(() => {
+        browser.refresh();
+        browser.sleep(short_timeout);
     });
 
-    it("should have the correct window title", () => {
-        expect(browser.getTitle()).toEqual('Blocks World');
+
+    describe("before starting the game", () => {
+        it("should activate the start button", () => {
+            expect(startButton.isEnabled()).toEqual(true);
+        });
+
+        it("should have the correct window title", () => {
+            expect(browser.getTitle()).toEqual('Blocks World');
+        });
     });
 
-    describe("After starting the game", () => {
+    describe("after starting the game", () => {
         beforeEach((done) => {
             startButton.click().then(done);
         });
