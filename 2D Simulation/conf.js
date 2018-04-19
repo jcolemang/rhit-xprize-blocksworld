cb357168e0988f2824c715eea44d4f6416353779
Lewis-Kelley <kelleyld@rose-hulman.edu>
2018-04-19 19:16:49 -0400

    Switching to Chrome.

--- 2D Simulation/conf.js
+++ 2D Simulation/conf.js
@@ -2,9 +2,5 @@ exports.config = {
     framework: 'jasmine',
     seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: ['e2eTesting.js'],
-    baseUrl: 'http://localhost:8000',
-    capabilities: {
-        browserName: 'firefox',
-        marionette: true
-    }
+    baseUrl: 'http://localhost:8000'
 }
