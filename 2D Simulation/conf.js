ce9e6590b9f47b9f09bb328e569e82eb3ee9bc2a
Lewis-Kelley <kelleyld@rose-hulman.edu>
2018-04-20 07:12:34 -0400

    Switched back to Firefox.

--- 2D Simulation/conf.js
+++ 2D Simulation/conf.js
@@ -2,5 +2,9 @@ exports.config = {
     framework: 'jasmine',
     seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: ['e2eTesting.js'],
-    baseUrl: 'http://localhost:8000'
+    baseUrl: 'http://localhost:8000',
+    capabilities: {
+        browserName: 'firefox',
+        marionette: true
+    }
 }
