ee1610019a6c43a4cfc979b2100d6fe5a16fdafd
Lewis-Kelley <kelleyld@rose-hulman.edu>
2018-04-21 13:25:40 -0400

    Switched to using jQuery-modal for the correction modals.

--- 2D Simulation/correctionUI.js
+++ 2D Simulation/correctionUI.js
@@ -1,35 +1,36 @@
 let correctionUI = new function () {
     let incorrect_button = $("#buttonIncorrect");
-
     let correctionsModal = $("#correctionsModal");
     let flipModal = $("#flipModal");
     let moveModal = $("#moveModal");
 
     incorrect_button.prop("disabled", true);
 
+    function openStickyModal(modalSelector) {
+        modalSelector.modal({
+            escapeClose: false,
+            clickClose: false,
+            showClose: false
+        });
+    }
+
     this.show_corrections_modal = function () {
-        correctionsModal.css("display", "block");
+        openStickyModal(correctionsModal);
     };
 
-    this.hide_corrections_modal = function () {
-        correctionsModal.css("display", "none");
-    };
+    this.hide_corrections_modal = $.modal.close;
 
     this.display_flip_explanation = function () {
-        flipModal.css("display", "block");
+        openStickyModal(flipModal);
     };
 
-    this.hide_flip_modal = function () {
-        flipModal.css("display", "none");
-    };
+    this.hide_flip_modal = $.modal.close;
 
     this.display_move_explanation = function () {
-        moveModal.css("display", "block");
+        openStickyModal(moveModal);
     };
 
-    this.hide_move_modal = function () {
-        moveModal.css("display", "none");
-    };
+    this.hide_move_modal = $.modal.close;
 
     this.disable_incorrect_button = function () {
         incorrect_button.prop("disabled", true);
