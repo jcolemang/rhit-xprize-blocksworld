aa8cd00578ebf0c91de62ea6001cc4fdaf7d2dc9
Lewis Kelley <kelleyld@rose-hulman.edu>
2018-04-24 08:10:11 -0400

    Testing moves corrector (#58)
    * Added tests for correct_flip.
    
    * Added tests for correct_move.
    
    * Added tests for _create_undo_move.
    
    * Added tests for _create_undo_flip.
    
    * Fixed error in previous commits.
    
    * Refactored "awaiting" variables to be fields.
    
    * Added tests for handling a user message.
    
    * Added tests regarding handling a flip correction.
    
    * Added tests for correcting a move.
    
    * Fixed small renaming bugs that crept in.
    
    * Added tests for running an undo action.
    
    * Added tests for starting to correct an action.
    
    * Reshuffled the functions to have a more logical ordering.
    
    * Added tests for updating the undo action.
    
    * Changed the _undo_action to be null when invalidated.

--- 2D Simulation/movesCorrector.js
+++ 2D Simulation/movesCorrector.js
@@ -1,102 +1,110 @@
-let movesCorrector = new function () {
-    let undo_action;
-    let awaiting_flip_correction = false;
-    let awaiting_move_correction = false;
+function MovesCorrector() {
+    this._awaiting_flip_correction = false;
+    this._awaiting_move_correction = false;
+    this._undo_action = null;
 
-    function run_undo_action() {
-        if (undo_action === undefined) {
-            return;
-        }
-
-        if (undo_action.type === "move") {
-            update_gui_block(undo_action);
-            update_score(undo_action);
-        } else if (undo_action.type === "flip") {
-            let id = undo_action.block_id.substring(5);
-
-            flipBlock(undo_action.block_id,
-                      blocks.get_block_text(id),
-                      blocks.get_block_color(id),
-                      currentConfig);
-        }
-
-        undo_action = undefined;
+    this.update_undo_move = function (moveData) {
+        this._undo_action = this._create_undo_move(moveData);
     }
 
-    this._start_correct_action = function () {
-        correctionUI.hide_corrections_modal();
-        correctionUI.disable_incorrect_button();
+    this._create_undo_move = function (moveData) {
+        let id = Number(moveData.block_id.substring(5));
 
-        run_undo_action();
+        return {
+            type: "move",
+            block_id: moveData.block_id,
+            left: blocks.get_block_left_pos(id),
+            top: blocks.get_block_top_pos(id)
+        };
+    }
 
-        blocks.display_block_ids();
+    this.update_undo_flip = function (block_id) {
+        this._undo_action = this._create_undo_flip(block_id);
+    };
+
+    this._create_undo_flip = function (block_id) {
+        return {
+            type: "flip",
+            block_id: block_id
+        };
     }
 
     this.correct_flip = function () {
-        awaiting_flip_correction = true;
+        this._awaiting_flip_correction = true;
         this._start_correct_action();
 
         correctionUI.display_flip_explanation();
     };
 
     this.correct_move = function () {
-        awaiting_move_correction = true;
+        this._awaiting_move_correction = true;
         this._start_correct_action();
 
         correctionUI.display_move_explanation();
     };
 
-    this.update_undo_move = function (moveData) {
-        let id = Number(moveData.block_id.substring(5));
+    this._start_correct_action = function () {
+        correctionUI.hide_corrections_modal();
+        correctionUI.disable_incorrect_button();
 
-        undo_action = {
-            type: "move",
-            block_id: moveData.block_id,
-            left: blocks.get_block_left_pos(id),
-            top: blocks.get_block_top_pos(id)
-        };
+        this._run_undo_action();
+
+        blocks.display_block_ids();
     };
 
-    this.update_undo_flip = function (block_id) {
-        undo_action = {
-            type: "flip",
-            block_id: block_id
-        };
+    this._run_undo_action = function () {
+        if (this._undo_action === null) {
+            return;
+        }
+
+        if (this._undo_action.type === "move") {
+            update_gui_block(this._undo_action);
+            update_score(this._undo_action);
+        } else if (this._undo_action.type === "flip") {
+            let id = this._undo_action.block_id.substring(5);
+
+            flipBlock(this._undo_action.block_id,
+                      blocks.get_block_text(id),
+                      blocks.get_block_color(id),
+                      currentConfig);
+        }
+
+        this._undo_action = null;
     };
 
     this.handle_message = function (message) {
         message = message.trim();
 
-        if (awaiting_flip_correction) {
-            handle_flip_message(message);
+        if (this._awaiting_flip_correction) {
+            this._handle_flip_message(message);
             return true;
-        } else if (awaiting_move_correction) {
-            handle_move_message(message);
+        } else if (this._awaiting_move_correction) {
+            this._handle_move_message(message);
             return true;
         } else {
             return false;
         }
     };
 
-    function handle_flip_message(message) {
+    this._handle_flip_message = function (message) {
         let id = Number(message);
 
-        if (message !== "" && is_valid_id(id)) {
+        if (message !== "" && this._is_valid_id(id)) {
             flipBlock("block" + id,
                       blocks.get_block_text(id),
                       blocks.get_block_color(id),
                       currentConfig);
-            awaiting_flip_correction = false;
+            this._awaiting_flip_correction = false;
             blocks.display_block_letters();
         } else {
             correctionUI.display_flip_explanation();
         }
     }
 
-    function handle_move_message(message) {
+    this._handle_move_message = function (message) {
         let id = Number(message);
 
-        if (message !== "" && is_valid_id(id)) {
+        if (message !== "" && this._is_valid_id(id)) {
             let gesture_pos = get_gesture_position();
             let move = {
                 left: gesture_pos.left,
@@ -107,14 +115,21 @@ let movesCorrector = new function () {
             update_gui_block(move);
             hide_gesture();
 
-            awaiting_move_correction = false;
+            this._awaiting_move_correction = false;
             blocks.display_block_letters();
         } else {
-            blocks.display_flip_explanation();
+            correctionUI.display_move_explanation();
         }
     }
 
-    function is_valid_id(id) {
+    this._is_valid_id = function (id) {
         return id % 1 === 0 && id >= 0 && id < NumBlocks;
     }
 };
+
+if (typeof module !== 'undefined'
+    && module.hasOwnProperty('exports')) {
+    module.exports = MovesCorrector;
+}
+
+let movesCorrector = new MovesCorrector();
