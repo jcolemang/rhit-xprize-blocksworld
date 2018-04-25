3cf9c3cd89ee34cc6f5af400dd92bece003cc1f2
Lewis-Kelley <kelleyld@rose-hulman.edu>
2018-04-25 15:49:11 -0400

    Added install, build, and run scripts.

---
+++ test.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+PYTHON="python3"
+
+function server_tests() {
+    cd server
+
+    $PYTHON -m pytest . --ignore=rhit-xprize-neural-network
+    return $?
+}
+
+function client_tests() {
+    cd ../2D\ Simulation
+
+    npx jasmine
+    return $?
+}
+
+function gui_tests() {
+    node_modules/protractor/bin/webdriver-manager start &
+    WEBDRIVER_PID=$!
+
+    cd ../server
+    $PYTHON simulation.py &
+    SIMULATION_PID=$!
+
+    cd ../2D\ Simulation
+    $PYTHON -m http.server &
+    SERVER_PID=$!
+
+    sleep 5
+
+    cd 2D\ Simulation
+    npx protractor conf.js
+    PROTRACTOR_RESULT=$?
+
+    kill $WEBDRIVER_PID
+    kill $SIMULATION_PID
+    kill $SERVER_PID
+
+    return $PROTRACTOR_RESULT
+}
+
+server_tests && client_tests && gui_tests
