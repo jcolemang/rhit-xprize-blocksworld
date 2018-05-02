dfee974cd823253bb9bd0c190d49261c60dc6915
Lewis-Kelley <kelleyld@rose-hulman.edu>
2018-05-02 16:41:06 -0400

    Fixed broken tests.

--- 2D Simulation/initBlocks.js
+++ 2D Simulation/initBlocks.js
@@ -6,23 +6,10 @@ function initAll() {
         currentConfig = getInitialConfiguration(possibleColors, possibleLetters, NumBlocks);
         finalBlocks = getFinalConfiguration(currentConfig);
     } else {
-        currentConfig = [{
-            topLetter: 'A',
-            bottomLetter: 'B',
-            topColor: 'red',
-            bottomColor: 'green',
-            left: 50,
-            top: 40,
-            id: 0
-        }];
+        currentConfig = loadFixedInitConfig();
+        finalBlocks = loadFixedGoalConfig();
 
-        finalBlocks = [{
-            topLetter: 'B',
-            topColor: 'green',
-            left: 10,
-            top: 20,
-            id: 0
-        }];
+        NumBlocks = currentConfig.length;
     }
 
     initBlocks(currentConfig);
---
+++ 2D Simulation/loadFixedConfig.js
@@ -0,0 +1,21 @@
+function loadFixedInitConfig() {
+    return [{
+        topLetter: 'A',
+        bottomLetter: 'B',
+        topColor: 'red',
+        bottomColor: 'green',
+        left: 50,
+        top: 40,
+        id: 0
+    }];
+}
+
+function loadFixedGoalConfig() {
+    return [{
+        topLetter: 'B',
+        topColor: 'green',
+        left: 10,
+        top: 20,
+        id: 0
+    }];
+}
